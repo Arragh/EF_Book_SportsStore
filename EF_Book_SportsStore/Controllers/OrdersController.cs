04012aa8bb9a6f792f09196d3e316b809f84574c
arragh@mail.ru <arragh@mail.ru>
2021-04-02 00:58:59 +0300

    Глава 7

--- EF_Book_SportsStore/Controllers/OrdersController.cs
+++ EF_Book_SportsStore/Controllers/OrdersController.cs
@@ -17,9 +17,37 @@ namespace EF_Book_SportsStore.Controllers
             this.ordersRepository = ordersRepository;
         }
 
-        public IActionResult Index()
+        public IActionResult Index() => View(ordersRepository.Orders);
+
+        public IActionResult EditOrder(Guid orderId)
         {
-            return View();
+            var products = productRepository.Products;
+            Order order = orderId == default(Guid) ? new Order() : ordersRepository.GetOrder(orderId);
+            IDictionary<Guid, OrderLine> linesMap = order.Lines?.ToDictionary(l => l.ProductId) ?? new Dictionary<Guid, OrderLine>();
+            ViewBag.Lines = products.Select(p => linesMap.ContainsKey(p.ProductId) ? linesMap[p.ProductId] : new OrderLine { Product = p, ProductId = p.ProductId, Quantity = 0 });
+            return View(order);
+        }
+
+        [HttpPost]
+        public IActionResult AddUpdateOrder(Order order)
+        {
+            order.Lines = order.Lines.Where(l => l.OrderLineId != default(Guid) || (l.OrderLineId == default(Guid) && l.Quantity > 0)).ToArray();
+            if (order.OrderId == default(Guid))
+            {
+                ordersRepository.AddOrder(order);
+            }
+            else
+            {
+                ordersRepository.UpdateOrder(order);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteOrder(Order order)
+        {
+            ordersRepository.DeleteOrder(order);
+            return RedirectToAction("Index");
         }
     }
 }
